510bdf334dc9cb3a167278ac73543f165679d4e6
migsmo <97015261+migsmo@users.noreply.github.com>
2024-05-08 19:24:06 +0800

    FIX prop parameters in heading-1.typography.jsx

--- src/components/typography/heading-1.typography.jsx
+++ src/components/typography/heading-1.typography.jsx
@@ -1,22 +1,20 @@
 import { Text } from '@react-pdf/renderer';
 import React from 'react';
 
-function Heading1({ children }, style) {
+function Heading1({ children, style }) {
   return (
-    <>
-      <Text
-        style={[
-          {
-            fontSize: 10,
-            fontFamily: 'SourceSansBold',
-            letterSpacing: '0',
-          },
-          style,
-        ]}
-      >
-        {children}
-      </Text>
-    </>
+    <Text
+      style={[
+        {
+          fontSize: 10,
+          fontFamily: 'SourceSansBold',
+          letterSpacing: '0',
+        },
+        style,
+      ]}
+    >
+      {children}
+    </Text>
   );
 }
 
