1de3dc35b62be7d62f4d4e8a535331884f9abffb
migsmo <97015261+migsmo@users.noreply.github.com>
2024-05-10 14:02:41 +0800

    REFACTOR BoxInput

--- src/components/inputs/box.input.jsx
+++ src/components/inputs/box.input.jsx
@@ -6,19 +6,19 @@ const styles = StyleSheet.create({
     height: 12,
     borderTopWidth: 1,
     borderBottomWidth: 1,
-    borderLeftWidth: 0, // No left border for middle squares
-    borderRightWidth: 1, // Right border on all squares
+    borderLeftWidth: 0,
+    borderRightWidth: 1,
     borderColor: '#000',
   },
   firstSquare: {
-    borderLeftWidth: 1, // Add left border to the first square
+    borderLeftWidth: 1,
   },
   squaresContainer: {
     flexDirection: 'row',
   },
 });
 
-function BoxInput({ x, y, number, style }) {
+function BoxInput({ number, style }) {
   return (
     <View style={[styles.squaresContainer, style]}>
       {Array.from({ length: number }, (_, index) => (
@@ -32,24 +32,3 @@ function BoxInput({ x, y, number, style }) {
 }
 
 export default BoxInput;
-
-// <Canvas
-//   paint={(painter) => {
-//     const squareSize = 12; // Size of the squares
-//     const spacing = 2; // Spacing between the squares
-//     for (let i = 0; i < number; i++) {
-//       painter
-//         .rect(x + (squareSize + spacing) * i, y, squareSize, squareSize)
-//         .stroke();
-//     }
-//   }}
-//   debug
-//   style={{
-//     position: 'absolute',
-//     top: y,
-//     left: x,
-//     width: number * (12 + 2) - 2, // Total width of all squares and spacings
-//     height: 12,
-//     zIndex: 1,
-//   }}
-// />
